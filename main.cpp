b7e6ec20061f7de5eee812477552d044c7e8c767
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2026-01-26 17:31:48 -0300

    Implement Dijkstra's algorithm and path reconstruction

--- main.cpp
+++ main.cpp
@@ -9,4 +9,36 @@ int main(int argc, char *argv[])
     QFile file;
     Vertices vertices = ler_vertices(&file);
     free(vertices.vertices);
+
+
+    double* distancias = dijkstra(&vertices, origem, predecessores);
+
+    if (distancias == NULL) {
+        printf("Erro ao calcular Dijkstra.\n");
+        return 1;
+    }
+
+    int tamanhoCaminho;
+    uint64_t* caminho = reconstruirCaminho(origem, destino, predecessores, &vertices, &tamanhoCaminho);
+
+    if (caminho != NULL) {
+        printf("Caminho encontrado com %d vértices:\n", tamanhoCaminho);
+        for (int i = 0; i < tamanhoCaminho; i++) {
+            printf("%lu", caminho[i]);
+            if (i < tamanhoCaminho - 1) printf(" -> ");
+        }
+        printf("\n");
+        free(caminho);
+         } else {
+        printf("Não há caminho.\n");
+    }
+
+    // Liberar memória
+    free(distancias);
+
+    liberarListasArestas(&vertices);
+    free(vertices.vertices);
+
+    return 0;
+
 }
