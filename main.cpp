d6ab0deff3b48304ea6a9a51db1bda69405c2d1f
Raphael Ambrosio <48883322+arkelangelo@users.noreply.github.com>
2026-02-02 10:58:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,29 @@
 int main(int argc, char *argv[])
 {
     QFile file;
+
     Vertices vertices = ler_vertices(&file);
     ler_arestas(&vertices, &file);
 
-    Distancia *distancias = calcular_distancias_dijkstra(&vertices, vertices.vertices[0].id);
+    ArvoreTrie *trie = criar_trie();
+
+    // falta implementar
+   //  - ler label_to_nodes.json
+  //   - para cada (nome, id):
+ //    -  inserir_na_trie(trie, nome, id);
+     
+
+    Distancia *distancias =
+        calcular_distancias_dijkstra(&vertices, vertices.vertices[0].id);
+
     Caminho caminho = construir_caminho_dijkstra(&vertices,
                                                  vertices.vertices[0].id,
                                                  vertices.vertices[8080].id,
                                                  distancias);
 
-    for (int i = vertices.quantidade - caminho.tamanho; i < vertices.quantidade; i++) {
+    for (int i = vertices.quantidade - caminho.tamanho;
+         i < vertices.quantidade;
+         i++) {
         printf("%lu ", caminho.caminho[i]);
     }
 
@@ -24,6 +37,7 @@ int main(int argc, char *argv[])
 
     free(distancias);
     free(caminho.caminho);
+    liberar_trie(trie);
     liberar_vertices(&vertices);
 
     return 0;
